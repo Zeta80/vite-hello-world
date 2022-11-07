1313bda69d23bfcc6bc8b52539f1cd90296d6fce
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-07 14:01:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+
+<script >
+import AppJumbo from "./components/AppJumbo.vue";
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!"
+    }
+  },
+  components: {
+    AppJumbo
+  },
+  methods: {
+    saluta() {
+      alert("ciao");
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+
+  <AppJumbo />
+
+</template>
+
+<style >
+
+</style>
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+            imgName: `326271.png`
+        }
+    },
+    methods: {
+        getImagePath() {
+            return new URL(`../assets/${this.imgName}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+<template>
+    <div class="jumbo">
+        <img :src="getImagePath()" alt="">
+    </div>
+</template>
+<style scoped>
+.jumbo {
+    width: 100%;
+    height: 60vh;
+}
+
+.jumbo img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
